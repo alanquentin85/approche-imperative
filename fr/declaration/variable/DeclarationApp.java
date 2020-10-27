1a1505548cd3b89d61830b70dd23b1a2c6346d65
alanquentin85 <alan?quentin@epsi.fr>
2020-10-27 14:07:47 +0100

    First commit

---
+++ fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,16 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args){
+        byte a = 8;
+        float b = 57.33F ;
+        short c = 3;
+        double d = 56.44D;
+        int e = 87;
+        boolean f = false;
+        long g = 465436436L;
+        String h = "Hello";
+        String randomString = "Voici le résultat d’un calcul :" + System.getProperty("line.separator") + "1+5=6";
+        System.out.println(randomString);
+    }
+}
